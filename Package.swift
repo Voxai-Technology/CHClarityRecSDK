308d02da052f39df4266eb7de38bc5e448fe84c5
VoxAISoftwareIOS <Software_IOS@vox-ai.com>
2026-01-09 19:07:31 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CHClarityRecSDK",
-            url: "https://gitee.com/superwangdg/chclarity-rec-sdkfor-ios/raw/0.1.2/CHClarityRecSDK.xcframework.zip",
+            url: "https://github.com/Voxai-Technology/CHClarityRecSDK/raw/CHClarityRecSDK.xcframework.zip",
             checksum: "d7fb85403be84c17127a35f901544c0948fde90c579b38742662ea2163e8683d"
         )
     ]
