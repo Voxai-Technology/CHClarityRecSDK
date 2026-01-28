5ba41eeb03b23c6f27eeb70cda05827c2835203c
VoxAISoftwareIOS <Software_IOS@vox-ai.com>
2026-01-28 16:18:54 +0800

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
         .binaryTarget(
             name: "CHClarityRecSDK",
             url: "https://github.com/Voxai-Technology/CHClarityRecSDK/releases/download/0.0.13/CHClarityRecSDK.xcframework.zip",
-            checksum: "1bc85338623cb8b0a7d3ac9167f41ad2da9c745bd4fd75ad4fa64e807275d019"
+            checksum: "48f5b14a8c09de1081b6f2d373c20954a0cdb3a428c4e7f1ba29139be785f5ce"
         )
     ]
 )
