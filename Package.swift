7c267e4c8d746c7f8e401018fc1aff78c7c8b879
Software_Zou <voxcloudm@vox-ai.com>
2026-03-10 16:46:41 +0800

    feat: 1. Compatible with firmware v1.00.25. 2. OTA upgrade removes the sandbox and is handled by the viewmodel for copying.

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "CHClarityRecSDK",
-            url: "https://github.com/Voxai-Technology/CHClarityRecSDK/releases/download/0.0.22/CHClarityRecSDK.xcframework.zip",
+            url: "https://github.com/Voxai-Technology/CHClarityRecSDK/releases/download/0.0.23/CHClarityRecSDK.xcframework.zip",
             checksum: "sha256:bf75b0dbd1cc13cc45869da0e54e9e83fbed3a226bf069a5a3b7c62d8367dfbb"
         )
     ]
